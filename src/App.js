3e6920776ec3a5c7c4cf8a40e83d6020e31440e9
JESSICA GALARZA <jessicaga941@gmail.com>
2022-10-02 22:11:47 -0400

    added landgin page image, and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
+import LandingPage from './Component/LandingPage/LandingPage';
 
 function App() {
   return (
   <div>
-
+<LandingPage />
   </div>
   );
 }
---
+++ src/Component/LandingPage/LandingPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Image1 from "../../assets/movie.night.png";
+import "./landingpage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-img">
+      <img src={Image1} className="img1" alt="" />
+      <div>
+        <h1>
+          {" "}
+          Imagine if
+ <br  />
+         <span> Snapchat</span>
+          <br />
+       
+          had events </h1>
+       
+        <button>
+      Create my event
+    </button>
+        <br />
+<p>Easily host and share events with your friends across any social media</p>
+        <div>
+    <button>
+     🎉 Create my event
+    </button>
+        </div>
+      </div>
+      {/* <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button> */}
+
+    </div>
+  );
+}
+
+export default LandingPage;
